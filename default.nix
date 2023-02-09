c57f6c72632f0ede114576bcebd08ff95599f681
Connor Feeley <git@cfeeley.org>
2023-02-08 23:41:21 -0500

    toronto-backgrounds: init at 0.0.1

--- default.nix
+++ default.nix
@@ -17,5 +17,7 @@
   apple_complete = pkgs.callPackage ./pkgs/os-specific/darwin/apple_complete/default.nix { };
   maclaunch = pkgs.callPackage ./pkgs/os-specific/darwin/maclaunch/default.nix { };
 
+  toronto-backgrounds = pkgs.callPackage ./pkgs/data/misc/toronto-backgrounds/default.nix { };
+
   qemu-xilinx = pkgs.callPackage ./pkgs/applications/virtualization/qemu/default.nix { };
 }
