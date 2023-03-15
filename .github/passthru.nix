34220075f8766a4d4bf91ccca92087031a73e419
Connor Feeley <git@cfeeley.org>
2023-03-15 09:00:00 -0400

    github: only build cacheOutputs

--- .github/passthru.nix
+++ .github/passthru.nix
@@ -5,7 +5,7 @@
 # Find all passthru.tests and return them as a list of full attribute paths
 builtins.toJSON (
   let
-    p = import ../. { };
+    p = import ../ci.nix { };
   in
   builtins.foldl'
     (acc: v:
@@ -13,5 +13,5 @@ builtins.toJSON (
       tests = builtins.attrNames (p.${v}.passthru.tests or { });
     in
     acc ++ map (t: "${v}.passthru.tests.${t}") tests) [ ]
-    (builtins.attrNames p)
+    (builtins.attrNames p.cachePkgs)
 )
