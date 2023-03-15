e3a343aaec793c30f777e9e87309fedfa1b7d585
Connor Feeley <git@cfeeley.org>
2023-03-15 13:02:12 -0400

    betterdisplay: init at 1.4.6

--- darwin-packages.nix
+++ darwin-packages.nix
@@ -10,6 +10,7 @@ lib.makeScope newScope (self: let inherit (self) callPackage; in {
   # installApplication = callPackage ./lib/installApplication.nix { };
 
   apple_complete = callPackage ./pkgs/os-specific/darwin/apple_complete { };
+  betterdisplay = callPackage ./pkgs/os-specific/darwin/betterdisplay { };
   launchcontrol = callPackage ./pkgs/os-specific/darwin/launchcontrol { };
   maclaunch = callPackage ./pkgs/os-specific/darwin/maclaunch { };
   sloth = callPackage ./pkgs/applications/misc/sloth { inherit (pkgs.darwin.apple_sdk_11_0.frameworks) Foundation; };
