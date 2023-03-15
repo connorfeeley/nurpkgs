8128ca7b7bd7efb0ae8d71374c4093f492eaa5b6
Connor Feeley <git@cfeeley.org>
2023-03-15 18:35:15 -0400

    flake: use current system nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
   description = "My personal NUR repository";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = { self, nixpkgs, flake-utils }:
     let
