eff504dc076d2f47e1553f3c653b644a8a35b853
Connor Feeley <git@cfeeley.org>
2023-03-22 09:00:00 -0400

    linux-xlnx: general cleanup

--- linux-kernels.nix
+++ linux-kernels.nix
@@ -24,40 +24,9 @@
 let
   kernels = recurseIntoAttrs (lib.makeExtensible (self:
     let callPackage = newScope self; in {
-      kernelPatches = callPackage ./pkgs/os-specific/linux/kernel/patches.nix { };
-      linux_xlnx = callPackage ./pkgs/os-specific/linux/kernel/xilinx-kernels.nix {
-        buildPackages = buildPackages // { stdenv = buildPackages.gcc10Stdenv; };
-      };
-      # linux_xlnx = callPackage ./pkgs/os-specific/linux/kernel/linux.nix {
-      #   buildPackages = buildPackages // { stdenv = buildPackages.gcc10Stdenv; };
-      # };
-
-  linuxConfig = {
-    src,
-    version ? (builtins.parseDrvName src.name).version,
-    makeTarget ? "defconfig",
-    name ? "kernel.config",
-  }: stdenvNoCC.mkDerivation {
-    inherit name src;
-    depsBuildBuild = [ buildPackages.stdenv.cc ]
-      ++ lib.optionals (lib.versionAtLeast version "4.16") [ buildPackages.bison buildPackages.flex ];
-    postPatch = ''
-      patchShebangs scripts/
-    '';
-    buildPhase = ''
-      set -x
-      make \
-        ARCH=${stdenv.hostPlatform.linuxArch} \
-        HOSTCC=${buildPackages.stdenv.cc.targetPrefix}gcc \
-        ${makeTarget}
-    '';
-    installPhase = ''
-      cp .config $out
-    '';
-  };
+      linux_xlnx = callPackage ./pkgs/os-specific/linux/kernel/xilinx-kernels.nix { };
     }));
 in
 {
   linux_xlnx = recurseIntoAttrs (linuxPackagesFor kernels.linux_xlnx);
-  kernelPatches = recurseIntoAttrs (linuxPackagesFor kernels.linux_xlnx.kernelPatches);
 }
