eef80cbed3d5086d67bfe26738e2b9e2dff7db32
Connor Feeley <git@cfeeley.org>
2023-03-28 09:00:00 -0400

    rclone-tui: init at 0.0.3

--- default.nix
+++ default.nix
@@ -32,6 +32,7 @@ let
       inherit linuxKernel;
       llama-cpp = callPackage ./pkgs/development/libraries/llama-cpp { inherit (pkgs.darwin.apple_sdk.frameworks) Accelerate; };
       nmos-cpp = callPackage ./pkgs/development/libraries/nmos-cpp { inherit cpprestsdk; };
+      rclone-tui = callPackage ./pkgs/applications/misc/rclone-tui { };
       xilinx-qemu = pkgs.callPackage ./pkgs/applications/virtualization/xilinx-qemu { inherit (pkgs.darwin.apple_sdk.frameworks) CoreAudio Cocoa; inherit (pkgs.darwin.stubs) rez setfile; };
       inherit tests;
       toronto-backgrounds = callPackage ./pkgs/data/misc/toronto-backgrounds { };
