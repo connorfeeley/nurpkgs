08eae229c150a8552aa6a77de66930b0c73f98c7
Connor Feeley <git@cfeeley.org>
2023-06-07 09:00:00 -0700

    tinkertool: init at 230420

--- darwin-packages.nix
+++ darwin-packages.nix
@@ -12,7 +12,8 @@ lib.makeScope newScope (self: let inherit (self) callPackage; in {
   apple_complete = callPackage ./pkgs/os-specific/darwin/apple_complete { };
   betterdisplay = callPackage ./pkgs/os-specific/darwin/betterdisplay { };
   launchcontrol = callPackage ./pkgs/os-specific/darwin/launchcontrol { };
-  maclaunch = callPackage ./pkgs/os-specific/darwin/maclaunch { };
   mac-stats = callPackage ./pkgs/os-specific/darwin/mac-stats { };
+  maclaunch = callPackage ./pkgs/os-specific/darwin/maclaunch { };
   sloth = callPackage ./pkgs/applications/misc/sloth { inherit (pkgs.darwin.apple_sdk_11_0.frameworks) Foundation; };
+  tinkertool = callPackage ./pkgs/os-specific/darwin/tinkertool { };
 })
