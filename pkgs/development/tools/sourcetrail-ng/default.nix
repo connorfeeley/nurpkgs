48625ab470823ae12afeb7e1f5a0c268db333973
Connor Feeley <git@cfeeley.org>
2024-05-17 09:00:00 -0400

    Revert "sourcetrail-ng: update"
    This reverts commit 27134f94abb4c31b184a0feb75a4cc705a175021.

--- pkgs/development/tools/sourcetrail-ng/default.nix
+++ pkgs/development/tools/sourcetrail-ng/default.nix
@@ -25,8 +25,6 @@
 , gtest
 , catch2_3
 , trompeloeil
-, conan
-, ninja
 }:
 
 let
@@ -51,7 +49,7 @@ let
   };
 
   ver = {
-    version = "3cfbfa62a6341617cb5d7fc008a8c85ff6c42051";
+    version = "8f7f43ad880b13230ea9ffdc7c323dbedf2cd5d0";
     # Fields must not have leading a zero
     year = "2023";
     month = "8";
@@ -67,7 +65,7 @@ stdenv.mkDerivation rec {
     owner = "OpenSourceSourceTrail";
     repo = "Sourcetrail";
     rev = version;
-    hash = "sha256-WCtCkQsXrN42KLRo/zGmWKnqmO86NObqMA/lmN6ZOzY=";
+    hash = "sha256-ThwuC/xm9RDF0m545Uii1FioGUFRpOJn8ElxRUmJAbw=";
     fetchSubmodules = true;
   };
 
@@ -96,8 +94,6 @@ stdenv.mkDerivation rec {
     sqlite
     tinyxml
     fmt.dev
-    conan
-    ninja
   ] ++ lib.optionals doCheck [
     gtest.dev
     catch2_3
